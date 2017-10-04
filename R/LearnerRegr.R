9c3f15f0de63c24c1c03fecc4f287d4d4b01cecd
Giuseppe Casalicchio <giuseppe.casalicchio@stat.uni-muenchen.de>
2017-10-04 11:15:48 +0200

    Learner api light (#18)
    * try to add predict.type
    
    * fix predict.type setting
    
    * allow passing of par vals
    
    * fix paramset, predicttype, parvals
    
    * write some helpers
    
    * cforest
    
    * fix bug
    
    * add model.extractors and cleanup cforest shit
    
    * fix test
    
    * added learners and helpers for classif.ksvm, classif.glmnet
    
    * fix bug gettaskdata
    
    * clean up stuff
    
    * reset to master
    
    * add description
    
    * fix properties

--- R/LearnerRegr.R
+++ R/LearnerRegr.R
@@ -4,7 +4,7 @@ LearnerRegr = R6Class("LearnerRegr",
   inherit = Learner,
   public = list(
     initialize = function(type, name, par.set, par.vals = list(), packages = character(0L), properties = character(0L), train, predict, model.extractors = list()) {
-      super$initialize(type, name, par.set, par.vals = list(), packages = character(0L), properties = character(0L), train, predict, model.extractors,
+      super$initialize(type, name, par.set, par.vals, packages, properties, train, predict, model.extractors,
         allowed.predict.types = c("response", "se"), predict.type = "response")
     }
   )
