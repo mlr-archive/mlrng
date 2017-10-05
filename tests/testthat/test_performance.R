c2138cedfbb7c31a0470f472630647770a0d382d
Bernd Bischl <bernd_bischl@gmx.net>
2017-10-05 13:25:35 +0300

    new AB for PredictResult and better perf tests

--- tests/testthat/test_performance.R
+++ tests/testthat/test_performance.R
@@ -7,6 +7,6 @@ test_that("performance", {
   pred = predict(model, task, subset = 1:150)
   p = performance(pred, measures = list(mlr.measures$get("mmce")))
   pv = p$perf.vals
-  expect_numeric(pv, len = 1L, names = "unique" )
+  expect_numeric(pv, len = 1L, names = "unique", finite = TRUE, any.missing = FALSE)
   expect_named(pv, "mmce")
 })
