3a074378fe6752a09ecb53ca0f0336edcd2ad067
Bernd Bischl <bernd_bischl@gmx.net>
2017-10-05 12:39:18 +0300

    tests: mock learners in extra file

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -124,36 +124,3 @@ expect_resampling = function(r, instantiated = NULL) {
 }
 
 
-# Dummy learner that can produce warnings/errors/messages
-lrn.mock.regr = LearnerRegr$new(
-  name = "mock",
-  par.set = ParamHelpers::makeParamSet(
-    ParamHelpers::makeDiscreteParam("method", values = c("mean", "median"), default = "mean"),
-    ParamHelpers::makeLogicalParam("message", default = FALSE),
-    ParamHelpers::makeLogicalParam("warning", default = FALSE),
-    ParamHelpers::makeLogicalParam("error", default = FALSE)
-  ),
-  par.vals = list(),
-  properties = c("missings", "factors", "numerics"),
-  train = function(task, subset, method = "mean", message = FALSE, warning = FALSE, error = FALSE,...) {
-    tn = unlist(task$data(subset, task$target))
-    mod = switch(method,
-      "mean" = mean(tn),
-      "median" = median(tn),
-      stop("Illegal value for 'method'"))
-    class(mod) = c("dummy.model", class(mod))
-    if (message)
-      message("dummy message")
-    if (warning)
-      warning("dummy warning")
-    if (error)
-      stop("dummy error")
-
-    mod
-  },
-
-  predict = function(model, task, subset, ...) {
-    as.numeric(model)
-  }
-)
-
