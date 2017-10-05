466aaf245afe04465077e46e018778374b41e6d3
Michel Lang <michellang@gmail.com>
2017-10-06 00:49:13 +0200

    fixes for nested

--- attic/nestedResampling.R
+++ attic/nestedResampling.R
@@ -2,27 +2,22 @@ getNestedResampling = function(outer, inner) {
   assertClass(outer, "Resampling")
   assertClass(inner, "Resampling")
 
-  Resampling$new(
+  r = Resampling$new(
     id = "nested resampling",
-    description = sprintf("nested resampling: [%s]x[%s]", outer$id, inner$id),
     instantiate = function(x) {
-      if (inherits(x, "Task"))
-        x = x$nrow
-      assertCount(x)
-
-      self$pars$outer$instantiate(x)
-      no = length(self$pars$outer)
-      ni = length(self$pars$inner)
+      self$pars$outer$instantiate(task)
+      no = self$pars$outer$iters
+      ni = self$pars$inner$iters
       tmp = vector("list", no * ni)
       self$instance = list(train = tmp, test = tmp)
 
       for (i in seq_len(no)) {
-        train = self$pars$outer$instance$train[[i]]
-        self$pars$inner$instantiate(sum(train))
+        train = self$pars$outer$train.set(i)
+        self$pars$inner$instantiate(length(train))
         for (j in seq_len(ni)) {
           ij = (i-1L) * ni + j
-          ind = as.bitwhich(train)
-          self$instance$train[[ij]] = replace(train, ind, self$pars$inner$instance$train[[j]])
+          # ind = as.bitwhich(train)
+          self$instance$train[[ij]] = intersect(train, self$pars$inner$train(j))
           self$instance$test[[ij]] = replace(train, ind, self$pars$inner$instance$test[[j]])
         }
       }
@@ -35,9 +30,9 @@ getNestedResampling = function(outer, inner) {
 
 test_that("nested resampling", {
   skip("Not finished yet")
-  task = Tasks$get("iris")
-  outer = Resamplings$get("cv")
-  inner = Resamplings$get("cv")
+  task = mlr.tasks$get("iris")
+  outer = mlr.resamplings$get("cv")
+  inner = mlr.resamplings$get("cv")
   inner$iters = 3
 
   r = getNestedResampling(outer, inner)
