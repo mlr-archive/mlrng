47dfd9328546f9e04aae258e522ff4147726cb15
Michel Lang <michellang@gmail.com>
2017-10-07 00:55:57 +0200

    fix predict

--- R/predict.R
+++ R/predict.R
@@ -16,7 +16,7 @@ predict.TrainResult = function(object, newdata = NULL, subset = NULL, ...) {
 }
 
 predictWorker = function(train.result, newdata, row.ids) {
-  learner = if (train.result$train.success) train.result$learner else createFallbackLearner(task)
+  learner = if (train.result$train.success) train.result$learner else createFallbackLearner(train.result$task)
   requireNS(learner$packages)
   pars = c(list(model = train.result, newdata = newdata), learner$par.vals)
   result = do.call(learner$predict, pars)
