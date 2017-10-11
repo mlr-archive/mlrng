bad3522dea6a136f623c62a5a51fc781bbc15ffd
Michel Lang <michellang@gmail.com>
2017-10-11 14:33:49 +0200

    use debugging index op in tests

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -2,6 +2,18 @@ library(testthat)
 library(checkmate)
 library(stringi)
 
+`[[.R6` = function(x, i, ...) {
+  if (!backports:::hasName(x, i))
+    stop("R6 class ", paste0(class(x), collapse = "/") ,"does not have slot '", i, "'!")
+  get(i, envir = x)
+}
+
+`$.R6` = function(x, name) {
+  if (!backports:::hasName(x, name))
+    stop("R6 class ", paste0(class(x), collapse = "/") ,"does not have slot '", name, "'!")
+  get(name, envir = x)
+}
+
 private = function(x) {
   if (!R6::is.R6(x))
     stop("Expected R6 class")
