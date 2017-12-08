f8da87f843985f2b073365873d79a2219e033a56
Michel Lang <michellang@gmail.com>
2017-12-08 11:10:48 +0100

    support config files

--- R/zzz.R
+++ R/zzz.R
@@ -16,35 +16,54 @@
 #' @keywords internal
 "_PACKAGE"
 
+
+mlrng = new.env(parent = emptyenv())
+mlrng$supported.col.types = c(
+  "logical", "integer", "numeric", "character", "factor", "ordered"
+)
+mlrng$supported.learner.props = c(
+  sprintf("feat.%s", mlrng$supported.col.types),
+  "missings", "weights", "twoclass", "multiclass", "prob", "featimp", "parallel",
+  "formula", "oobpreds", "se"
+)
+mlrng$result.states = c(
+  "Result", "TrainResult", "PredictResult", "PerformanceResult", "ResampleResult", "BenchmarkResult"
+)
+mlrng$default.opts = list(
+  mlrng.verbose = TRUE,
+  mlrng.debug = TRUE,
+  mlrng.keep.train.output = FALSE,
+  mlrng.continue.on.learner.error = FALSE,
+  mlrng.train.encapsulation = 1L
+)
+
 .onLoad = function(libname, pkgname) { #nocov start
   backports::import(pkgname)
   backports::import(pkgname, "hasName", force = TRUE)
   parallelMap::parallelRegisterLevels(package = "mlrng", levels = c("resample", "benchmark", "tune"))
 
+  # set default options
   opts = options()
-  opts.mlrng = list(
-    mlrng.verbose = TRUE,
-    mlrng.debug = TRUE,
-    mlrng.keep.train.output = FALSE,
-    mlrng.continue.on.learner.error = FALSE,
-    mlrng.train.encapsulation = 1
-  )
-  unset = !(names(opts.mlrng) %in% names(opts))
-  if (any(unset)) options(opts.mlrng[unset])
-} #nocov end
+  unset = which(!(names(mlrng$default.opts) %in% names(opts)))
+  if (length(unset))
+    options(mlrng$default.opts[unset])
 
+  # read config file
+  fn = file.path(rappdirs::user_config_dir("mlrng"), "config.yml")
+  if (file.exists(fn)) {
+    opts = try(yaml::read_yaml(fn))
+    if (inherits(opts, "try-error") || !testList(opts, names = "unique")) {
+      gwarn("Config file {fn} seems to be syntactically invalid")
+    } else {
+      names(opts) = sprintf("mlrng.%s", names(opts))
+      options(opts)
+    }
+  }
+
+} #nocov end
 
 .onUnload = function(libpath) { #nocov start
   pm.opts = getOption("parallelMap.registered.levels")
   pm.opts$mlrng = NULL
   options(parallelMap.registered.levels = pm.opts)
 } #nocov end
-
-mlrng = new.env(parent = emptyenv())
-mlrng$supported.col.types = c("logical", "integer", "numeric", "character", "factor", "ordered")
-mlrng$supported.learner.props = c(
-  sprintf("feat.%s", mlrng$supported.col.types),
-  "missings", "weights", "twoclass", "multiclass", "prob", "featimp", "parallel",
-  "formula", "oobpreds", "se"
-)
-mlrng$result.states = c("Result", "TrainResult", "PredictResult", "PerformanceResult", "ResampleResult", "BenchmarkResult")
