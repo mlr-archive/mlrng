d1427c12384ba9570fd47c213a3d8a2ed335a6a2
pat-s <patrick.schratz@gmail.com>
2018-07-12 08:44:51 +0200

    add tic setup

---
+++ tic.R
@@ -0,0 +1,12 @@
+get_stage("install") %>%
+  add_code_step(install.packages(old.packages())) %>%
+  add_code_step(devtools::install_github("pat-s/rcmdcheck@build-args")) %>% # FIXME: If this is solved in r-lib/rcmdcheck
+  add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
+
+get_stage("script") %>%
+  add_code_step(devtools::document()) %>%
+  add_step(step_rcmdcheck(notes_are_errors = FALSE, build_args = "--no-build-vignettes",
+                          check_args = "--ignore-vignettes --no-manual --as-cran"))
+
+get_stage("after_success") %>%
+  add_code_step(covr::codecov())
