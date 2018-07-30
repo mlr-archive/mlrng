2adf91ea6e142dcfe59458d8e7ba607f1f20323f
Patrick Schratz <patrick.schratz@gmail.com>
2018-07-30 15:07:51 +0200

    Add tic setup, auto-deploy docs, add code coverage (#69)
    * Test on Travis and appveyor using `tic` (as we do in `mlr`)
    * Auto-deploy `man/` and `NAMESPACE` changes 
    * Automatically create and deploy pkgdown site
    * Run code coverage on codecov and add badge to README

---
+++ tic.R
@@ -0,0 +1,28 @@
+get_stage("install") %>%
+  add_code_step(devtools::install_github("pat-s/rcmdcheck@build-args")) %>% # FIXME: If this is solved in r-lib/rcmdcheck
+  add_code_step(devtools::install_github("mlr-org/phng")) %>%
+  add_code_step(devtools::install_github("mllg/backports")) %>%
+  add_code_step(if (length(find.package("roxygen2", quiet = TRUE)) == 0) install.packages("roxygen2")) %>%
+  add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
+
+get_stage("script") %>%
+  add_code_step(devtools::document()) %>%
+  add_step(step_rcmdcheck(notes_are_errors = FALSE, warnings_are_errors = FALSE,
+                          build_args = "--no-build-vignettes",
+                          check_args = "--ignore-vignettes --no-manual --as-cran"))
+
+if (inherits(ci(), "TravisCI") && Sys.getenv("TRAVIS_R_VERSION_STRING") == "release") {
+
+  get_stage("before_deploy") %>%
+    add_step(step_setup_ssh())
+
+  get_stage("deploy") %>%
+    add_code_step(devtools::document(roclets=c('rd', 'collate', 'namespace'))) %>%
+    add_step(step_build_pkgdown()) %>%
+    add_step(step_push_deploy(orphan = TRUE, path = "docs", branch = "gh-pages")) %>%
+    add_step(step_push_deploy(orphan = FALSE, branch = "master", commit_paths = c("NAMESPACE", "man/*")))
+
+  get_stage("after_success") %>%
+    add_code_step(covr::codecov(quiet = FALSE))
+
+}
