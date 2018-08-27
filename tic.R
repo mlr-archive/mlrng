506595bc2109ed46cd2aed1b61d82e6820ffc265
Patrick Schratz <patrick.schratz@gmail.com>
2018-08-27 12:33:35 +0200

    update tic, tidy description (#70)

--- tic.R
+++ tic.R
@@ -1,9 +1,8 @@
 get_stage("install") %>%
   add_code_step(devtools::install_github("pat-s/rcmdcheck@build-args")) %>% # FIXME: If this is solved in r-lib/rcmdcheck
-  add_code_step(devtools::install_github("mlr-org/phng")) %>%
-  add_code_step(devtools::install_github("mllg/backports")) %>%
-  add_code_step(if (length(find.package("roxygen2", quiet = TRUE)) == 0) install.packages("roxygen2")) %>%
-  add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
+  add_step(step_install_cran("roxygen2")) %>%
+  add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE)) %>%
+  add_code_step(devtools::update_packages(TRUE))
 
 get_stage("script") %>%
   add_code_step(devtools::document()) %>%
@@ -19,8 +18,8 @@ if (inherits(ci(), "TravisCI") && Sys.getenv("TRAVIS_R_VERSION_STRING") == "rele
   get_stage("deploy") %>%
     add_code_step(devtools::document(roclets=c('rd', 'collate', 'namespace'))) %>%
     add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy(orphan = TRUE, path = "docs", branch = "gh-pages")) %>%
-    add_step(step_push_deploy(orphan = FALSE, branch = "master", commit_paths = c("NAMESPACE", "man/*")))
+    add_step(step_push_deploy(path = "docs", branch = "gh-pages")) %>%
+    add_step(step_push_deploy(branch = "master", commit_paths = c("NAMESPACE", "man/*")))
 
   get_stage("after_success") %>%
     add_code_step(covr::codecov(quiet = FALSE))
